4a50aaac4d39d1baa22b540d7c8ac743810e0775
Jon Tofant <75683804+JonTofant@users.noreply.github.com>
2021-05-21 17:34:55 +0200

    Added tablet mode

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
+//Deklaracija spremenljivk
 const gumbBarva = document.querySelector('.gumbBarva');
 const body = document.querySelector('body');
 let notranjiOkvirEna = document.querySelector('.ena');
 let notranjiOkvirDva = document.querySelector('.dve');
 let notranjiOkvirTri = document.querySelector('.tri');
-let clicked =true;
+let clicked =true; //Tema je v osnovi svetla
+let temaOzadja=window.localStorage.getItem('tema');
+
+if(temaOzadja){
+    if(temaOzadja=='temna'){
+        temno()
+
+    }
+
+    else{
+        svetlo()
+    }
+}
+
+
 gumbBarva.addEventListener('click', function (e){
+    //Posluša za klik na gumb
     if(!clicked){
         
         svetlo()
@@ -15,17 +31,23 @@ gumbBarva.addEventListener('click', function (e){
     
 });
 
+
 function svetlo(){
+    //Svetla tema nadzorne plošče
     body.style.backgroundColor='#add9db'
     clicked= true
     notranjiOkvirEna.style.backgroundColor='white'
     notranjiOkvirDva.style.backgroundColor='white'
     notranjiOkvirTri.style.backgroundColor='white'
+    window.localStorage.setItem('tema', 'svetla');
 }
 function temno(){
+    //Temna tema nadzorne plošče
     body.style.backgroundColor='#1521d3'
     clicked=false
     notranjiOkvirEna.style.backgroundColor='darkGrey'
     notranjiOkvirDva.style.backgroundColor='darkGrey'
     notranjiOkvirTri.style.backgroundColor='darkGrey'
+    window.localStorage.setItem('tema', 'temna');
 }
+

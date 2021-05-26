8b82de2fe185ae4f06ad312a9c12cacac2f81ad7
Jon Tofant <75683804+JonTofant@users.noreply.github.com>
2021-05-26 09:41:25 +0200

    divided js in to many functions

--- index.js
+++ index.js
@@ -1,71 +1,32 @@
-/*------------
+/*-----------------------
 Deklaracija spremenljivk
------------------*/
+-------------------------*/
 
+ //Tema je v osnovi svetla
+ let clicked =true;
+ //temaOzadja dobi vrednost shranjeno iz prejšnjega obiska strani
+let temaOzadja=window.localStorage.getItem('tema');
+
+/*-------
+THE DOM
+--------*/
 const slikaGumbBarva = document.querySelector('.slikaGumbBarva')
 const gumbBarva = document.querySelector('.gumbBarva');
 const body = document.querySelector('body');
 let notranjiOkvirEna = document.querySelector('.ena');
 let notranjiOkvirDva = document.querySelector('.dve');
-let clicked =true; //Tema je v osnovi svetla
-//temaOzadja dobi vrednost shranjeno iz prejšnjega obiska strani
-let temaOzadja=window.localStorage.getItem('tema');
-//Preveri če tema ozadja obstaja
-if(temaOzadja){
-    //Če tema ozadja obstaja in je ta temna vklopi temen način
-    if(temaOzadja=='temna'){
-        temno()
-
-    }
-    //Drugače vklopi svetel način
-    else{
-        svetlo()
-    }
-}
 
 
-gumbBarva.addEventListener('click', function (e){
-    //Posluša za klik na gumb
-    if(!clicked){
-        
-        svetlo()
-        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-    }
-    else{
-        temno()
-        var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
-        
-    }
-    
-});
-
-/*------------
-Svetli način
+/*---------------
+Glavni program
 -----------------*/
 
-function svetlo(){
-    body.style.backgroundColor='#add9db'
-    clicked= true
-    notranjiOkvirEna.style.backgroundColor='white'
-    notranjiOkvirDva.style.backgroundColor='white'
-    //Shrani izbiro uporabnika v localstorage
-    window.localStorage.setItem('tema', 'svetla');
-    slikaGumbBarva.src='slike/lightMode.svg'
-}
+preverjanjeOzadja();
 
-/*------------
-Temni način
------------------*/
+gumbBarvaClick();
 
-function temno(){
-    body.style.backgroundColor='#606060'
-    clicked=false
-    notranjiOkvirEna.style.backgroundColor='darkGrey'
-    notranjiOkvirDva.style.backgroundColor='darkGrey'
-    //Shrani izbiro uporabnika v localstorage
-    window.localStorage.setItem('tema', 'temna');
-    slikaGumbBarva.src='slike/darkoMode.svg'
-}
+
+
 
 /*-------
 Graf
@@ -108,5 +69,73 @@ google.load('visualization', '1', {
     }
     window.onload = resize();
     window.onresize = resize;
+  
+  }
+
+  /*--------------
+  Gumb za barvo ozadja
+  ----------------*/
+
+  function gumbBarvaClick(){
+    gumbBarva.addEventListener('click', function (e){
+        //Posluša za klik na gumb
+        if(!clicked){ 
+            svetlo()
+        }
+        else{
+            temno()            
+        }
+        
+    });
+    
+    }
+
+
+/*------------
+Svetli način
+-----------------*/
+
+function svetlo(){
+    body.style.backgroundColor='#add9db'
+    clicked= true
+    notranjiOkvirEna.style.backgroundColor='white'
+    notranjiOkvirDva.style.backgroundColor='white'
+    //Shrani izbiro uporabnika v localstorage
+    window.localStorage.setItem('tema', 'svetla');
+    slikaGumbBarva.src='slike/lightMode.svg'
+}
+
+/*------------
+Temni način
+-----------------*/
+
+function temno(){
+  body.style.backgroundColor='#606060'
+  clicked=false
+  notranjiOkvirEna.style.backgroundColor='darkGrey'
+  notranjiOkvirDva.style.backgroundColor='darkGrey'
+  //Shrani izbiro uporabnika v localstorage
+  window.localStorage.setItem('tema', 'temna');
+  slikaGumbBarva.src='slike/darkoMode.svg'
+}
+
+/*-----------------------------
+Preverjanje shranjenega ozadja
+-------------------------------*/
+
+function preverjanjeOzadja(){
+
+  //Preveri če tema ozadja obstaja
+    if(temaOzadja){
+        //Če tema ozadja obstaja in je ta tema temna vklopi temni način
+        if(temaOzadja=='temna'){
+            temno()
+  
+        }
+        //Drugače vklopi svetel način
+        else{
+            svetlo()
+        }
+    }
   
   }
